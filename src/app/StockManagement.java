18f1bfbfc6e00d0b0ce02b752d4d695e4c9a9358
SinYing <chansy@1utar.my>
2025-05-01 00:30:13 +0800

    Add selectProducts()

--- src/app/StockManagement.java
+++ src/app/StockManagement.java
@@ -7,6 +7,8 @@ public class StockManagement {
 
 	private static Product[] products;
 	
+	
+	//Get the maximum number of products the user wishes to store in the system
 	public static int getMaxProducts(Scanner scanner) {
 		int maxProduct;
 		
@@ -26,6 +28,36 @@ public class StockManagement {
 	}	
 	
 	
+	//Display products[] and let user to select product that they want to update
+	public static int selectProducts(Scanner scanner) {
+		int index;
+		
+		if(products.length == 0) {
+			System.out.print("No any products added.");
+		}
+		
+		System.out.println("\n--- Product List ---");
+		
+		for (int i = 0; i < products.length; i++) {
+			System.out.println(i + ": " + products[i].getName());
+		}
+		
+		do {
+			System.out.print("Select product index: ");
+			while (!scanner.hasNextInt()) {
+				System.out.print("Invalid input. Enter a valid product index: ");
+				scanner.nextInt();
+			}
+			
+		index = scanner.nextInt();
+		System.out.println("");
+		
+		}while(index < 0 || index >= products.length);
+		
+		return index;
+			
+		}
+	
 	
 
 
@@ -35,8 +67,10 @@ public class StockManagement {
 	
 	public static void main(String[] args) {
 		
-		
-		
+		Scanner scanner = new Scanner(System.in);
+		int maxProducts = getMaxProducts(scanner);
+		products = new Product[maxProducts];
+				
 	}
 
 }
