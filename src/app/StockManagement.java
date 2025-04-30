8fe9c2a646f3ddc577104b4d1dec7d8e44957afb
SinYing <chansy@1utar.my>
2025-04-30 23:51:58 +0800

    Add getMaxProduct()

--- src/app/StockManagement.java
+++ src/app/StockManagement.java
@@ -1,10 +1,42 @@
 package app;
 
+import java.util.Scanner;
+
 public class StockManagement {
 
+
+	private static Product[] products;
+	
+	public static int getMaxProducts(Scanner scanner) {
+		int maxProduct;
+		
+		do {			
+			System.out.print("Enter the number of products to store: ");
+		
+			while(!scanner.hasNextInt()) {
+				System.out.print("Invalid input! Please enter an positive integer: ");
+				scanner.nextInt();
+			 }
+			 
+			 maxProduct = scanner.nextInt();
+			 System.out.println("");
+			 
+		 }while(maxProduct < 0);
+		return maxProduct;
+	}	
+	
+	
+	
+
+
+
+		
+	   
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, world.testing");
+		
+		
+		
 	}
 
 }
---
+++ src/app/TV.java
@@ -0,0 +1,62 @@
+package app;
+// subclass of the Product class
+
+public class TV extends Product{
+	//additional instance variable
+	private String screenType;
+	private String resolution;
+	private int displaySize;
+	
+	//parameterized constructor
+	public TV(String name, double price, int quantity_available, String product_id, String screenType,
+			String resolution, int displaySize) {
+		super(name, price, quantity_available, product_id);
+		this.screenType = screenType;
+		this.resolution = resolution;
+		this.displaySize = displaySize;
+	}
+
+	//Getter & Setter
+	public String getScreenType() {
+		return screenType;
+	}
+
+	public void setScreenType(String screenType) {
+		this.screenType = screenType;
+	}
+
+	public String getResolution() {
+		return resolution;
+	}
+
+	public void setResolution(String resolution) {
+		this.resolution = resolution;
+	}
+
+	public int getDisplaySize() {
+		return displaySize;
+	}
+
+	public void setDisplaySize(int displaySize) {
+		this.displaySize = displaySize;
+	}
+	
+	 //method
+	public double calculateStockValue()
+	{
+		return super.totalInventoryValue();
+	}
+
+	@Override
+	public String toString() {
+		return "Item number         : " + super.getProduct_id()
+				+ "\nProduct name        : " + super.getName() 
+				+ "\nScreenType          : " + screenType 
+				+ "\nResolution          : " + resolution 
+				+ "\nDisplay size        : " + displaySize
+				+ "\nQuantity available  : " + super.getQuantity_available()
+				+ "\nPrice (RM)          : " + super.getPrice()
+				+ "\nInventory value (RM): " + super.totalInventoryValue()
+				+ "\nProduct status      : " + (super.isStatus()? "Active" : "Discontinue");
+	}
+}
